56fd0a49b33226115e52d5a75d717b3a21a43339
Angga Adji Surya <anggas25@live.com>
2021-01-23 19:30:11 +0700

    Refactor

---
+++ src/sections/WidgetPost.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import Swal from 'sweetalert2'
+import {getNews} from "../services/apis/newsApi";
+import loading from '../assets/img/loading.gif'
+import EmptyCard from "../components/EmptyCard";
+
+class WidgetPost extends Component {
+
+    state = {
+        newsData: [],
+        isLoaded: false
+    }
+
+    businessToday = () => {
+        getNews("business")
+            .then((res) => {
+                this.setState({
+                    newsData: res.articles,
+                    isLoaded: true
+                })
+            }).catch(() => {
+            Swal.fire("Oops", "Connection Timeout !!!", "error")
+        })
+    }
+
+    componentDidMount() {
+        this.businessToday()
+    }
+
+    render() {
+        const data = this.state.newsData
+        return (
+
+            <EmptyCard title={"Bisnis Hari ini"}>
+                {this.state.isLoaded ?
+                    <div className="row">
+                        {data.map(businessData =>
+                            <div className="col-12 px-4 pb-4 ">
+                                <div>
+                                    <a href={businessData.url}><img src={businessData.urlToImage} alt=""
+                                                                    className="img-fluid widgetPostImage"/></a>
+                                </div>
+                                <div className="pt-3">
+                                    <a href={businessData.url}><p
+                                        className="font-weight-bold">{businessData.title}</p></a>
+                                </div>
+                                <hr/>
+                            </div>
+                        )}
+
+                    </div>
+                    :
+                    <div className="row justify-content-md-center">
+                        <div className="col-md-auto">
+                            <img src={loading} alt="" className="img-fluid" width="28px"/>
+                        </div>
+                    </div>
+                }
+            </EmptyCard>
+        );
+    }
+}
+
+export default WidgetPost;
--- src/sections/WidgetSection.js
+++ src/sections/WidgetSection.js
@@ -1,89 +1,76 @@
 import React, {Component} from 'react';
-import WidgetPost from "../components/WidgetPost";
+import WidgetPost from "./WidgetPost";
 import {socialLink} from "../variables/SocialLink";
 import {navigationLink} from "../variables/NavigationLink";
 import {sitePartner} from "../variables/SitesData";
+import EmptyCard from "../components/EmptyCard";
 
 class WidgetSection extends Component {
     render() {
         return (
             <div className="row">
                 <div className="col-12 my-3">
-                    <div className="card">
-                        <div className="card-body">
-                            <a href="https://panel.niagahoster.co.id/ref/106487" target="_blank"><img
-                                src="https://panel.niagahoster.co.id/banners/Set3-niagahoster-300x250.png"
-                                alt="Hosting Unlimited Indonesia" border="0" width="300" height="250"
-                                className="img-fluid"/></a>
-                        </div>
-                    </div>
+                    <EmptyCard>
+                        <a href="https://panel.niagahoster.co.id/ref/106487" target="_blank"><img
+                            src="https://panel.niagahoster.co.id/banners/Set3-niagahoster-300x250.png"
+                            alt="Hosting Unlimited Indonesia" border="0" width="300" height="250"
+                            className="img-fluid"/></a>
+                    </EmptyCard>
                 </div>
                 <div className="col-12 my-3">
                     <WidgetPost/>
                 </div>
                 <div className="col-12 my-3">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="card-title font-weight-bold">Kategori</div>
-                            <div className="row">
-                                {navigationLink.map(linkData =>
-                                    <div className="col mb-2">
-                                        <a href={linkData.url}
-                                           className="btn-sm btn-block btn btn-outline-primary rounded-pill text-center font-weight-light">{linkData.title}
-                                        </a>
-                                    </div>
-                                )}
-                            </div>
+                    <EmptyCard title={"Kategori"}>
+                        <div className="row">
+                            {navigationLink.map(linkData =>
+                                <div className="col mb-2">
+                                    <a href={linkData.url}
+                                       className="btn-sm btn-block btn btn-outline-primary rounded-pill text-center font-weight-light">{linkData.title}
+                                    </a>
+                                </div>
+                            )}
                         </div>
-                    </div>
+                    </EmptyCard>
                 </div>
                 <div className="col-12 my-3">
-                    <div className="card">
-                        <div className="card-body">
-                            <a rel="nofollow" href="https://invol.co/cl2a7lg"><img
-                                src="https://img.involve.asia/rpss/campaigns_banners/50470-Qvmo1qFDnOqfIT73I6CImCeJ0RDqG9tS.jpg"
-                                className="img-fluid"/></a>
-                        </div>
-                    </div>
+                    <EmptyCard>
+                        <a rel="nofollow" href="https://invol.co/cl2a7lg"><img
+                            src="https://img.involve.asia/rpss/campaigns_banners/50470-Qvmo1qFDnOqfIT73I6CImCeJ0RDqG9tS.jpg"
+                            className="img-fluid"/></a>
+                    </EmptyCard>
                 </div>
                 <div className="col-12 my-3">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="card-title font-weight-bold">Kerjasama</div>
-                            <div className="row">
-                                {sitePartner.map(partnerData =>
-                                    <div className="col-6">
-                                        <a href={partnerData.url}
-                                           className="btn-sm btn-block btn btn-outline-primary rounded-pill text-center font-weight-light">{partnerData.title}
-                                        </a>
-                                    </div>
-                                )}
-                            </div>
+                    <EmptyCard title={"Kerjasama"}>
+                        <div className="row">
+                            {sitePartner.map(partnerData =>
+                                <div className="col-6">
+                                    <a href={partnerData.url}
+                                       className="btn-sm btn-block btn btn-outline-primary rounded-pill text-center font-weight-light">{partnerData.title}
+                                    </a>
+                                </div>
+                            )}
                         </div>
-                    </div>
+                    </EmptyCard>
                 </div>
                 <div className="col-12 my-3">
-                    <div className="card">
-                        <div className="card-body">
-                            <a rel="nofollow" href="https://invol.co/cl2a7qy"><img
-                                src="https://img.involve.asia/rpss/campaigns_banners/50478-RLAH8gUy5w949UV1dhvHOCz6763rrACY.jpg"
-                                className="img-fluid"/></a>
-                        </div>
-                    </div>
+                    <EmptyCard>
+                        <a rel="nofollow" href="https://invol.co/cl2a7qy"><img
+                            src="https://img.involve.asia/rpss/campaigns_banners/50478-RLAH8gUy5w949UV1dhvHOCz6763rrACY.jpg"
+                            className="img-fluid"/></a>
+                    </EmptyCard>
+
                 </div>
                 <div className="col-12 my-3">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="card-title font-weight-bold">Follow</div>
-                            <div className="row">
-                                {socialLink.map(linkData =>
-                                    <div className="col"><a href={linkData.url}><h2 className={linkData.color}>
-                                        {linkData.icon}</h2></a>
-                                    </div>
-                                )}
-                            </div>
+                    <EmptyCard title={"Follow"}>
+                        <div className="row">
+                            {socialLink.map(linkData =>
+                                <div className="col"><a href={linkData.url}><h2 className={linkData.color}>
+                                    {linkData.icon}</h2></a>
+                                </div>
+                            )}
                         </div>
-                    </div>
+                    </EmptyCard>
                 </div>
             </div>
         );
